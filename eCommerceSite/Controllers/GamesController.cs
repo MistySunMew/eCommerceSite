d282f48af8d4b5a85829e69ff24eacbaad075098
MistySunMew <michael.e.forbes@comcast.net>
2023-01-30 09:44:13 -0800

    Create Game fully functional

---
+++ eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,39 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly VideoGameContext _context;
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Games.Add(game);
+                _context.SaveChanges();
+
+                ViewData["Message"] = $"{game.Title} added successfully!";
+
+                return View();
+            }
+
+            return View(game);
+        }
+    }
+}

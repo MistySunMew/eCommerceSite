32ed0aca40f9105ed51153f3e801285fda14cce2
MistySunMew <michael.e.forbes@comcast.net>
2023-02-19 15:37:52 -0800

    Created member class and added to context

---
+++ eCommerceSite/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
+{
+	public class Member
+	{
+		[Key]
+		public int MemberID { get; set; }
+
+		[Required] //This should be the primary key
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Username { get; set; }
+	}
+}

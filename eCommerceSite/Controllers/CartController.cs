ac3eea0cad2f7ad8ce51090cdd5ed2e64074ebe5
MistySunMew <michael.e.forbes@comcast.net>
2023-03-06 14:10:10 -0800

    Added add to cart button to catalog

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly VideoGameContext _context;
+
+        public CartController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Game? game = _context.Games.Where(g => g.GameID == id).SingleOrDefault();
+
+            if (game != null)
+            {
+                //Game with specified ID does not exist
+                TempData["Message"] = $"Sorry that game no longer exists";
+                return RedirectToAction("Index", "Games");
+            }
+
+            //TODO add game to cart
+            TempData["Message"] = $"{game.Title} added to cart";
+            return RedirectToAction("Index", "Games");
+        }
+    }
+}
